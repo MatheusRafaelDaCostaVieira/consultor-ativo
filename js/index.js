9df25b831a0b5c6ac64de3c7a54076332ff50810
MatheusRafaelDaCostaVieira <matheusrafael.pa@gmail.com>
2024-01-05 16:13:14 -0300

    Código aprimorado no geral, fechamento do menu por click encorpororado em uma função que minimiza os cards sobre nós quando o usuário clica em outro elemento feita.

--- js/index.js
+++ js/index.js
@@ -1,27 +1,70 @@
-let popUpDiv = $(".pop-up-div");
-let popUpP = $(".pop-up-p");
-let termsOfService = $(".terms-of-service");
+// The document element
+const documentElement = $(document);
+// The browser window
+const broserWindow = $(window);
+// html page body
+const body = $("body");
+/* Detect user device animation settings */
+const userAnimationSettings = window.matchMedia(
+  "(prefers-reduced-motion: reduce)"
+);
+// Elements that will be animate when the pages get loaded
+const hideBeforeLoad = $(".hide-before-load");
+// Navbar reference
+const navbar = $(".navbar");
+// Menu section
+const menuSideBar = $("#menu");
+// Menu side bar
+const menuContent = $(".menu-container");
+// Menu hamburguer icon
+const menuBtn = $(".menu-button");
+// Menu and navbar links
+const navigationLink = $(".navigation-link");
+// Menu cards
+const aboutCard = $(".about-card");
+// This is the sing up pop up blur div.
+const popUpDiv = $(".pop-up-div");
+// Pop up itself
+const singUpPopUp = $(".sing-up-pop-up");
+// The pop up paragraph.
+const popUpP = $(".pop-up-p");
+// The terms of service div.
+const termsOfService = $(".terms-of-service");
 
+// When the page gets loaded, these codes will get execulted.
 $(document).ready(function () {
-  /* And when the menu link get clicked */
-  $(".menu-links .navigation-link").on("click", function () {
+  setAnimations();
+  checkUserTheme();
+  changeIconsColor();
+  animationAfterLoad();
+  activeNavbar();
+  scrollAnimation();
+
+  // Call functions when user clicks at the page
+  body.on("click", function (e) {
+    menuClickCheck(e.target);
+    aboutCardClickCheck(e.target);
+  });
+
+  // And when the menu link get clicked
+  navigationLink.on("click", function () {
     closeMenu();
   });
 
-  /* Hides the menu if the screen is bigger than 900px wide */
-  $(window).on("resize", function () {
-    if (screen.width > 900) {
+  // Hides the menu if the screen is bigger than 900px wide
+  broserWindow.on("resize", function () {
+    if (broserWindow.innerWidth() > 900) {
       closeMenu();
     }
   });
 
-  /* Actives when user scrolls */
-  $(document).on("scroll", function () {
+  // Actives when user scrolls
+  documentElement.on("scroll", function () {
     activeNavbar();
   });
 
-  /* Calls the function that adds the hover class when the user hovers and/or clicks on the card */
-  $(".about-card").on("click", function (event) {
+  // Calls the function that adds the hover class when the user hovers and/or clicks on the card
+  aboutCard.on("click", function (event) {
     /* Gets the card that was clicked */
     clickedAboutCard = event.currentTarget;
 
@@ -31,55 +74,36 @@ $(document).ready(function () {
       clickedAboutCard.classList.remove("about-card-hover");
     } else {
       /* Remove the class of the active card - and from all of them */
-      $(".about-card").removeClass("about-card-hover");
+      aboutCard.removeClass("about-card-hover");
 
       /* Adds the class to the clicked card */
       clickedAboutCard.classList.add("about-card-hover");
     }
   });
 
-  /* If the card was cliked in a desktop, this eventListener prevents it to stays in hovered state if it was cliked while hovered */
-  $(".about-card").on("mouseleave", function (event) {
+  // If the card was cliked in a desktop, this eventListener prevents it to
+  // stays in hovered state if it was cliked while hovered
+  aboutCard.on("mouseleave", function (event) {
     event.currentTarget.classList.remove("about-card-hover");
   });
 
-  $("body").on("click", function (e) {
-    const menu = document.querySelector("#menu");
-    const menuBtn = document.querySelector(".menu-button");
+  // Call the scroll animation function when user scrolls
+  window.addEventListener("scroll", scrollAnimation);
+});
 
-    if (menu.classList.contains("active-menu")) {
-      if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
-        closeMenu();
-      }
-    }
-  });
+// After the page got loaded, animate elements with class hide-before-load
+function animationAfterLoad() {
+  hideBeforeLoad.addClass("animation-after-load");
+}
 
-  /* Detects the user theme to change icons color */
+// Check users's color theme
+function checkUserTheme() {
   window
     .matchMedia("(prefers-color-scheme: dark)")
     .addEventListener("change", changeIconsColor);
-
-  /* Detect user device animation settings */
-  const query = window.matchMedia("(prefers-reduced-motion: reduce)");
-  if (query.matches) {
-    $(":root").css("--transition", "linear");
-  }
-
-  /* Call the scroll animation function when user scrolls */
-  window.addEventListener("scroll", scrollAnimation);
-
-  animationAfterLoad();
-  changeIconsColor();
-  activeNavbar();
-  scrollAnimation();
-});
-
-/* Animates elements with the class hide-before-load after load */
-function animationAfterLoad() {
-  $(".hide-before-load").addClass("animation-after-load");
 }
 
-/* Changes the icon according to the user's theme */
+// Changes the icon according to the user's theme
 function changeIconsColor() {
   const userTheme = window.matchMedia("(prefers-color-scheme: dark)");
 
@@ -100,19 +124,19 @@ function changeIconsColor() {
   }
 }
 
-/* If scroolTop is over 0, adds active-navbar and active-navbar-menu classes */
+// If scroolTop is more than 0, adds active-navbar and active-navbar-menu classes
 function activeNavbar() {
-  if ($(document).scrollTop() > 0 || $("body").scrollTop() > 0) {
-    $(".navbar").addClass("active-navbar");
-    $(".menu-container").addClass("active-navbar-menu");
+  if ($(document).scrollTop() > 0 || body.scrollTop() > 0) {
+    navbar.addClass("active-navbar");
+    menuContent.addClass("active-navbar-menu");
   } else {
-    /* If it's 0, then remove them */
-    $(".navbar").removeClass("active-navbar");
-    $(".menu-container").removeClass("active-navbar-menu");
+    /* If it's not (so it's zero) remove them */
+    navbar.removeClass("active-navbar");
+    menuContent.removeClass("active-navbar-menu");
   }
 }
 
-/* Animates elements with class scroll-animation */
+// Animates elements with class scroll-animation
 function scrollAnimation() {
   let scrollAnimationElement = document.querySelectorAll(".scroll-animation");
 
@@ -129,43 +153,54 @@ function scrollAnimation() {
   }
 }
 
-/* Toggle the menu between open and closed */
+// Toggle the menu between open and closed
 function toggleMenu() {
-  $("body").toggleClass("block-scroll");
-  $("#menu").toggleClass("active-menu");
-  $(".navbar").toggleClass("active-menu-navbar");
+  body.toggleClass("block-scroll");
+  menuSideBar.toggleClass("active-menu");
+  navbar.toggleClass("active-menu-navbar");
 }
 
-/* Open the menu */
-function openMenu() {
-  $("body").addClass("block-scroll");
-  $("#menu").addClass("active-menu");
-  $(".navbar").addClass("active-menu-navbar");
-}
-
-/* Close the menu, remove navbar white background and unblock the page's scroll */
+// Close the menu, remove navbar white background and unblock the page's scroll
 function closeMenu() {
-  $("body").removeClass("block-scroll");
-  $("#menu").removeClass("active-menu");
-  $(".navbar").removeClass("active-menu-navbar");
+  body.removeClass("block-scroll");
+  menuSideBar.removeClass("active-menu");
+  navbar.removeClass("active-menu-navbar");
 }
 
-/* Makes the pop up appear when user sings up */
-function togglePopUp() {
-  let popUpWindow = $(".sing-up-pop-up");
-  if (popUpWindow.hasClass("active")) {
-    popUpWindow.removeClass("active");
-  } else {
-    popUpWindow.addClass("active");
+// Checks if user is clicking out of the menu
+// if yes, the menu gets closed
+function menuClickCheck(clickedItem) {
+  if (menuSideBar.hasClass("active-menu")) {
+    if (
+      !menuSideBar[0].contains(clickedItem) &&
+      !menuBtn[0].contains(clickedItem)
+    ) {
+      closeMenu();
+    }
   }
 }
 
-/* Display the terms of service */
-function openTermsOfService() {
-  /*   let popUpHeight = popUpDiv.css("height");
-  popUpHeight = Number(popUpHeight.substring(0, popUpHeight.length - 2));
-  popUpHeight = popUpHeight + 120 + "px"; */
+// Checks if user is clicking out of the about cards
+// if yes, the cards get back to the normal.
+function aboutCardClickCheck(clickedItem) {
+  for (let card of aboutCard) {
+    if (!card.contains(clickedItem)) {
+      card.classList.remove("about-card-hover");
+    }
+  }
+}
 
+// Makes the pop up appear when user sings up
+function togglePopUp() {
+  if (singUpPopUp.hasClass("active")) {
+    singUpPopUp.removeClass("active");
+  } else {
+    singUpPopUp.addClass("active");
+  }
+}
+
+// Display the terms of service
+function openTermsOfService() {
   popUpDiv.css("--expanded-height", "calc(100% - 160px)");
   popUpDiv.css("--expanded-width", "100%");
 
@@ -174,11 +209,18 @@ function openTermsOfService() {
   popUpP.removeClass("active");
 }
 
-/* Hides the terms of service */
+// Hides the terms of service
 function closeTermsOfService() {
   setTimeout(() => {
     popUpDiv.removeClass("expanded");
     termsOfService.removeClass("active");
     popUpP.addClass("active");
   }, 300);
+}
+
+// Turn on/off animations according to user's settings
+function setAnimations() {
+  if (userAnimationSettings.matches) {
+    $(":root").css("--transition", "linear");
+  }
 }
